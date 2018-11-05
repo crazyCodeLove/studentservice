daac99848152972cbdb0f51205f5fc6aa5c3c6fb
zhaopengcheng <zhaopengcheng@idwzx.com>
2018-11-06 00:16:08 +0800

    dependence on api client

--- api/src/main/java/com/sse/api/StudentServiceApi.java
+++ api/src/main/java/com/sse/api/StudentServiceApi.java
@@ -2,6 +2,8 @@ package com.sse.api;
 
 import com.sse.model.RequestParamBase;
 import com.sse.model.Response;
+
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @date 2018-11-05 21:24
  */
 
+@FeignClient(value = "service-provider")
 public interface StudentServiceApi {
 
     @RequestMapping(value = "/student/1", method = RequestMethod.POST)
