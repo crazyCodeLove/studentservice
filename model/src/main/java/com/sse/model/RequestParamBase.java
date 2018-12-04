05ae5339dbfd6b013bdac88fad16908347e10639
zhaopengcheng <zhaopengcheng@idwzx.com>
2018-12-04 23:31:48 +0800

    add log

--- model/src/main/java/com/sse/model/RequestParamBase.java
+++ model/src/main/java/com/sse/model/RequestParamBase.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -19,7 +20,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class RequestParamBase {
+public class RequestParamBase implements Serializable {
 
     @NotNull(message = "id 不能为空")
     private Integer id;
---
+++ service-provider/src/main/java/com/sse/config/LogParamAspect.java
@@ -0,0 +1,54 @@
+package com.sse.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author ZHAOPENGCHENG
+ * @email
+ * @date 2018-12-04 22:14
+ */
+
+@Configuration
+@Aspect
+@Slf4j
+public class LogParamAspect {
+
+    @Pointcut("execution(* com.sse.controller..*.* (..))")
+    public void controllerPoint() {
+    }
+
+    @Around("controllerPoint()")
+    public Object aroundController(ProceedingJoinPoint point) throws Throwable {
+        long beginTime = System.currentTimeMillis();
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+        /** 通用的请求数据 */
+        log.info("url:" + request.getRequestURL());
+        log.info("method:" + request.getMethod());
+        log.info("query string:" + request.getQueryString());
+        log.info("ip:" + request.getRemoteAddr());
+
+        /** 处理方法数据 */
+        log.info("class:" + point.getTarget().getClass().getName());
+        log.info("method:" + point.getSignature().getName());
+        log.info("param:" ,point.getArgs());
+
+
+        Object result = point.proceed();
+        long time = System.currentTimeMillis() - beginTime;
+        log.info("cost time(ms):" + time);
+        return result;
+    }
+
+
+}
