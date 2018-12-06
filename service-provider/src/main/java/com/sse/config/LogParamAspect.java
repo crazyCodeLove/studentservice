1cbbb83cdc4f0159136669129e2000e6cbc6d1d8
pczhao <crazyCodeLove@163.com>
2018-12-06 15:08:15 +0800

    add ip log

--- service-provider/src/main/java/com/sse/config/LogParamAspect.java
+++ service-provider/src/main/java/com/sse/config/LogParamAspect.java
@@ -1,5 +1,8 @@
 package com.sse.config;
 
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sse.util.IpUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -10,6 +13,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 /**
  * @author ZHAOPENGCHENG
@@ -21,6 +25,8 @@ import javax.servlet.http.HttpServletRequest;
 @Aspect
 @Slf4j
 public class LogParamAspect {
+    ThreadLocal<StringBuilder> logHandler = new ThreadLocal<>();
+    ThreadLocal<Long> startTime = new ThreadLocal<>();
 
     @Pointcut("execution(* com.sse.controller..*.* (..))")
     public void controllerPoint() {
@@ -28,24 +34,44 @@ public class LogParamAspect {
 
     @Around("controllerPoint()")
     public Object aroundController(ProceedingJoinPoint point) throws Throwable {
-        long beginTime = System.currentTimeMillis();
+        startTime.set(System.currentTimeMillis());
         ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = requestAttributes.getRequest();
+        logHandler.set(new StringBuilder());
         /** 通用的请求数据 */
-        log.info("url:" + request.getRequestURL());
-        log.info("method:" + request.getMethod());
-        log.info("query string:" + request.getQueryString());
-        log.info("ip:" + request.getRemoteAddr());
+        logHandler.get().append("session ID:");
+        logHandler.get().append(request.getSession().getId());
+        logHandler.get().append("; url:");
+        logHandler.get().append(request.getRequestURL());
+        logHandler.get().append("; method:");
+        logHandler.get().append(request.getMethod());
+        logHandler.get().append("; query string:");
+        logHandler.get().append(request.getQueryString());
+        logHandler.get().append("; ip:");
+        logHandler.get().append(IpUtil.getIpAddr(request));
 
         /** 处理方法数据 */
-        log.info("class:" + point.getTarget().getClass().getName());
-        log.info("method:" + point.getSignature().getName());
-        log.info("param:", point.getArgs());
-
+        logHandler.get().append("; class:");
+        logHandler.get().append(point.getTarget().getClass().getName());
+        logHandler.get().append("; method:");
+        logHandler.get().append(point.getSignature().getName());
+        logHandler.get().append("; param:");
+        logHandler.get().append(Arrays.toString(point.getArgs()));
+        log.info(logHandler.get().toString());
+        logHandler.remove();
 
         Object result = point.proceed();
-        long time = System.currentTimeMillis() - beginTime;
-        log.info("cost time(ms):" + time);
+
+        logHandler.set(new StringBuilder());
+        logHandler.get().append("session ID:");
+        logHandler.get().append(request.getSession().getId());
+        logHandler.get().append("; result:");
+        logHandler.get().append(result);
+        logHandler.get().append("; cost time(ms):");
+        logHandler.get().append(System.currentTimeMillis() - startTime.get());
+        startTime.remove();
+        log.info(logHandler.get().toString());
+        logHandler.remove();
         return result;
     }
 
---
+++ service-provider/src/main/java/com/sse/util/IpUtil.java
@@ -0,0 +1,56 @@
+package com.sse.util;
+
+import org.apache.commons.lang.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author pczhao
+ * @email
+ * @date 2018-12-06 14:05
+ */
+
+public class IpUtil {
+
+    public static String getIpAddr(HttpServletRequest request) {
+        if (null == request) {
+            return null;
+        }
+        String ipAddress = request.getHeader("x-forwarded-for");
+        if (StringUtils.isBlank(ipAddress) || StringUtils.equalsIgnoreCase("unknown", ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (StringUtils.isBlank(ipAddress) || StringUtils.equalsIgnoreCase("unknown", ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (StringUtils.isBlank(ipAddress) || StringUtils.equalsIgnoreCase("unknown", ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+            if (StringUtils.equals(ipAddress, "127.0.0.1") || StringUtils.equals(ipAddress, "0:0:0:0:0:0:0:1")) {
+                // 根据网卡取本机配置的IP
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    return null;
+                }
+                ipAddress = inet.getHostAddress();
+            }
+        }
+        // 对于通过多个代理的情况，第一个非unknown的有效IP字符串为客户端真实IP,多个IP按照','分割
+        if (!StringUtils.isBlank(ipAddress) && ipAddress.length() > 15) {
+            // "***.***.***.***".length() = 15
+            String[] ipArrs = StringUtils.split(ipAddress, ",");
+            for (String ipArr : ipArrs)
+            {
+                if (!StringUtils.isBlank(ipArr) && !StringUtils.equalsIgnoreCase("unknown", ipArr))
+                {
+                    ipAddress = ipArr;
+                    break;
+                }
+            }
+        }
+        return ipAddress;
+    }
+}
