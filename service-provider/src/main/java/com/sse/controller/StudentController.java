1243a2dbf77f10361a6d65a9f2e60174d159f9ab
pczhao <crazyCodeLove@163.com>
2018-12-06 15:22:50 +0800

    update validate

--- service-provider/src/main/java/com/sse/controller/StudentController.java
+++ service-provider/src/main/java/com/sse/controller/StudentController.java
@@ -2,8 +2,7 @@ package com.sse.controller;
 
 import com.sse.model.RequestParamBase;
 import com.sse.model.Response;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
+import com.sse.util.ValidateUtil;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,13 +19,8 @@ import javax.validation.Valid;
 public class StudentController {
 
     @RequestMapping(value = "/student/1", method = RequestMethod.POST)
-    public Response getStudent(@RequestBody @Valid RequestParamBase paramBase, BindingResult validResult) {
-        if (validResult.hasErrors()) {
-            for (ObjectError error : validResult.getAllErrors()) {
-                System.out.println(error.getDefaultMessage());
-            }
-            return null;
-        }
+    public Response getStudent(@RequestBody @Valid RequestParamBase paramBase) {
+        ValidateUtil.validate(paramBase);
         System.out.println(paramBase);
         Response result = Response.builder().age(23).name("well").build();
         return result;
