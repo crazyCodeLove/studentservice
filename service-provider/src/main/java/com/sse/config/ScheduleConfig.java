df3200577020a9d7a2177aa54a94abc55ca2b807
pczhao <crazyCodeLove@163.com>
2018-12-20 15:19:45 +0800

    add scheduling

---
+++ service-provider/src/main/java/com/sse/config/ScheduleConfig.java
@@ -0,0 +1,25 @@
+package com.sse.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executors;
+
+/**
+ * 添加调度系统配置。schedule 为每项任务分配一个线程
+ *
+ * @author pczhao
+ * @email
+ * @date 2018-12-20 14:21
+ */
+@Configuration
+public class ScheduleConfig implements SchedulingConfigurer {
+
+    private int THREAD_POOL_SIZE = 20;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(Executors.newScheduledThreadPool(THREAD_POOL_SIZE));
+    }
+}
--- service-provider/src/main/java/com/sse/service/ScheduleService.java
+++ service-provider/src/main/java/com/sse/service/ScheduleService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author ZHAOPENGCHENG
@@ -22,9 +23,15 @@ public class ScheduleService extends ServiceBase {
     /**
      * 秒 分 时 天 月 星期 ，以下表示每天每隔2小时执行一次
      */
-    @Scheduled(cron = "0 0 0/2 * * ?")
+    @Scheduled(cron = "0/1 * * * * ?")
     public void writeTime() {
-        System.out.println(sdf.format(new Date()));
+        System.out.println("thread ID:" + Thread.currentThread().getId() + ", time:" + sdf.format(new Date()));
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("thread ID:" + Thread.currentThread().getId() + " done");
     }
 
 }
