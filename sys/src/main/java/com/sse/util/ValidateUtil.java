30e6fc5641c2ef75bc799f308826a90fa3e88b89
pczhao <crazyCodeLove@163.com>
2019-04-02 19:10:02 +0800

    optimize validate

--- sys/src/main/java/com/sse/util/ValidateUtil.java
+++ sys/src/main/java/com/sse/util/ValidateUtil.java
@@ -1,5 +1,6 @@
 package com.sse.util;
 
+import com.sse.exception.ExceptionCodeEnum;
 import com.sse.exception.ParamRTException;
 import org.hibernate.validator.HibernateValidator;
 
@@ -31,6 +32,9 @@ public class ValidateUtil {
      * @param obj 待校验参数
      */
     public static <T> void validate(T obj) {
+        if (obj == null) {
+            throw new ParamRTException(ExceptionCodeEnum.PARAM_NULL_EXCEPTION, ExceptionCodeEnum.PARAM_NULL_EXCEPTION.getNote());
+        }
         Set<ConstraintViolation<T>> constraintViolations = validator.validate(obj);
         if (constraintViolations.size() > 0) {
             throw new ParamRTException(constraintViolations.iterator().next().getMessage());
