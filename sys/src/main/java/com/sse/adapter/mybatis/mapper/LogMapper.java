ea47b55aee2e0ae024cbf5a6553c035e4e34dd5d
pczhao <crazyCodeLove@163.com>
2019-06-28 13:24:57 +0800

    update mapper to Repository

--- sys/src/main/java/com/sse/adapter/mybatis/mapper/LogMapper.java
+++ sys/src/main/java/com/sse/adapter/mybatis/mapper/LogMapper.java
@@ -1,14 +1,14 @@
 package com.sse.adapter.mybatis.mapper;
 
 import com.sse.model.log.LogInfo;
-import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * author pczhao <br/>
  * date 2019-01-18 15:53
  */
 
-@Mapper
+@Repository
 public interface LogMapper {
     /**
      * 保存请求日志信息到数据库
